439bf4eb559b606fe7753e747188ab5e5f63bbdc
buke <wangbuke@gmail.com>
2014-04-03 01:13:58 +0800

    change color and title

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
+title GreenOpenERP - www.GreenOpenERP.com
+COLOR 0A
 SET PATH=%CD%\pgsql\bin;%CD%\python;%CD%\win32\wkhtmltopdf;%PATH%.
 "%CD%\pgsql\bin\"pg_ctl -D "%CD%/pgsql/data" -l "%CD%/pgsql/logfile" start
 CD nginx
