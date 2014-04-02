bba551a18071b181048d075956f95c1de72e075d
buke <wangbuke@gmail.com>
2014-04-03 01:13:15 +0800

    change color and title

--- register_pg_service.bat
+++ register_pg_service.bat
@@ -1,3 +1,5 @@
+title GreenOpenERP - www.GreenOpenERP.com
+COLOR 0A
 cd /d %~dp0
 "%CD%\pgsql\bin\"pg_ctl register -N "greenpostgresql" -D "%CD%/pgsql/data" -l "%CD%/pgsql/logfile"
 pause
--- unregister_pg_service.bat
+++ unregister_pg_service.bat
@@ -1,3 +1,5 @@
+title GreenOpenERP - www.GreenOpenERP.com
+COLOR 0A
 cd /d %~dp0
 "%CD%\pgsql\bin\"pg_ctl unregister -N "greenpostgresql"
 pause
